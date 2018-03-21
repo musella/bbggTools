a399913fe3a3bdb860823cc978e045402b80eefc
Pasquale Musella <pasquale.musella@cern.ch>
2018-03-21 11:23:42 +0100

    Fixed json name in run_workspaceStd.sh

--- test/RunJobs/run_workspaceStd.sh
+++ test/RunJobs/run_workspaceStd.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-fggRunJobs.py --load jsons/80X/SMHH_Moriond17_v1.json -d test --no-use-tarball  workspaceStd.py maxEvents=100 doubleHTagsOnly=True doDoubleHTag=True doSystematics=False dumpWorkspace=False dumpTrees=True
+fggRunJobs.py --load jsons/80X/Modiond17_flashgg_DoubleHTag.json -d test --no-use-tarball  workspaceStd.py maxEvents=100 doubleHTagsOnly=True doDoubleHTag=True doSystematics=False dumpWorkspace=False dumpTrees=True
 
